a2710311b21d93dc5af507dc0a7e024ced691d08
Seulgi Kim <manofaciton0@gmail.com>
2023-12-18 10:15:21 +0900

    2023-12-18

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+# models.py의 model class 상속.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
